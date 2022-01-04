645567c4f939e482c49276636d1ad219d0e4d7aa
Rotondwa Netshimbupfe <87181553+RotondwANetsh@users.noreply.github.com>
2022-01-04 16:03:49 +0200

    Sorting arrays using loops

---
+++ src/SortingArrays1.java
@@ -0,0 +1,12 @@
+public class SortingArrays1 {
+    public static void main(String[] args) {
+        int[] array = new int[50];
+
+        array[19] = 60;
+        array[20] = 61;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
